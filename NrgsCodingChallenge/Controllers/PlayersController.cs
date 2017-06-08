7ef7327df585aae9f70750518f53b0dca838a029
Dirk Rombauts <drombauts@greentube.com>
2017-06-08 16:10:52 +0200

    Make unit tests pass

--- NrgsCodingChallenge/Controllers/PlayersController.cs
+++ NrgsCodingChallenge/Controllers/PlayersController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NrgsCodingChallenge.Models;
 
 namespace NrgsCodingChallenge.Controllers
 {
@@ -11,14 +13,14 @@ namespace NrgsCodingChallenge.Controllers
     {
         // GET api/players
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<IEnumerable<string>> Get(CancellationToken cancellationToken)
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id:int}")]
-        public string GetById(int id)
+        public async Task<Player> GetById(int id, CancellationToken cancellationToken)
         {
             return id.ToString();
         }
@@ -26,7 +28,7 @@ namespace NrgsCodingChallenge.Controllers
         // GET api/values/byemail/someone@example.com
         // GET api/values/naughtiusmaximus
         [HttpGet("{emailornick}")]
-        public string GetByEmailOrNick(string emailornick)
+        public async Task<string> GetByEmailOrNick(string emailornick, CancellationToken cancellationToken)
         {
             return emailornick;
         }
@@ -39,19 +41,19 @@ namespace NrgsCodingChallenge.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task Post([FromBody]string value, CancellationToken cancellationToken)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task Put(int id, [FromBody]string value, CancellationToken cancellationToken)
         {
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task Delete(int id, CancellationToken cancellationToken)
         {
         }
     }
